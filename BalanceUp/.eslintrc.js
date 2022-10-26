0723035e865e5a18a15339e95376f2813d978946
Tkfrnfl <rul64@naver.com>
2022-10-27 01:16:56 +0900

    lint

--- BalanceUp/.eslintrc.js
+++ BalanceUp/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'spaced-comment': 'error',
+    'no-duplicate-imports': 'error',
+    'react-hooks/rules-of-hooks': 'warn', // Checks rules of Hooks
+  },
 };
