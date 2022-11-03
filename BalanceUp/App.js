b2fc608082a5189f3210e7fab2ad482e6267952f
Tkfrnfl <rul64@naver.com>
2022-11-03 17:47:08 +0900

    test naver login

--- BalanceUp/App.js
+++ BalanceUp/App.js
@@ -16,6 +16,8 @@ import {
   Text,
   useColorScheme,
   View,
+  Platform,
+  Alert,
 } from 'react-native';
 
 import {
@@ -25,9 +27,23 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {Button} from 'react-native';
+import {NaverLogin, getProfile} from '@react-native-seoul/naver-login';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
+// const {connectToDevTools} = require('react-devtools-core');
+// const wsInstance = connectToDevTools({
+//   host: 'localhost',
+//   port: 8097,
+//   /* This does NOT work */
+//   // websocket: (global as any).WebSocket,
+//   resolveRNStyle: null,
+//   isAppActive: () => true,
+// });
+// console.log('[app.ts] Got wsInstance:', wsInstance);
+// console.log("[app.ts] wsInstance's onopen was:", wsInstance.onopen);
+
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -53,14 +69,49 @@ const Section = ({children, title}): Node => {
     </View>
   );
 };
+const androidKeys = {
+  kConsumerKey: 'emLJacIpqC1VGarFjLHx',
+  kConsumerSecret: 'z_Q_8LbpiI',
+  kServiceAppName: 'keyum',
+};
+
+const initials = androidKeys;
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const [naverToken, setNaverToken] = React.useState(null);
 
+  const naverLogin = props => {
+    return new Promise((resolve, reject) => {
+      console.log('test');
+      NaverLogin.login(props, (err, token) => {
+        console.log(`\n\n  Token is fetched  :: ${token} \n\n`);
+        setNaverToken(token);
+        if (err) {
+          console.log(err);
+          reject(err);
+          return;
+        }
+        resolve(token);
+      });
+    });
+  };
+  const naverLogout = () => {
+    NaverLogin.logout();
+    setNaverToken('');
+  };
+
+  const getUserProfile = async () => {
+    const profileResult = await getProfile(naverToken.accessToken);
+    if (profileResult.resultcode === '024') {
+      Alert.alert('로그인 실패', profileResult.message);
+      return;
+    }
+    console.log('profileResult', profileResult);
+  };
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -76,8 +127,17 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            <Button
+              title="네이버 로그인"
+              onPress={() => naverLogin(initials)}
+            />
+            {!!naverToken && (
+              <Button title="로그아웃하기" onPress={naverLogout} />
+            )}
+
+            {!!naverToken && (
+              <Button title="회원정보 가져오기" onPress={getUserProfile} />
+            )}
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
