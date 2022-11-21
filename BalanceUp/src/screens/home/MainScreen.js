a301a84063f05ba77b2dbcde6a15f661f9a07ae9
hyeone999 <caps026@naver.com>
2022-11-21 09:46:00 +0900

    fix and delete

--- BalanceUp/src/screens/home/MainScreen.js
+++ BalanceUp/src/screens/home/MainScreen.js
@@ -7,9 +7,8 @@ import {
   SafeAreaView,
   ScrollView,
 } from 'react-native';
-import MainCarousel from '../../components/MainCarousel';
-import ProgressCircle from 'react-native-progress-circle';
 import commonStyles from '../../css/commonStyles';
+import ProgressCircle from 'react-native-progress-circle';
 
 const MainScreen = ({navigation}) => {
   function onStart() {
@@ -20,12 +19,6 @@ const MainScreen = ({navigation}) => {
       <ScrollView style={styles.scrollview}>
         <View style={styles.titleWrapper}>
           <Text style={styles.title1}>KEYUM</Text>
-          <MainCarousel> 캐러셀 임시 구현</MainCarousel>
-        </View>
-        <View style={styles.buttonWrapper}>
-          <TouchableOpacity style={styles.button} onPress={onStart}>
-            <Text style={styles.buttonText}>닉네임 만들기</Text>
-          </TouchableOpacity>
         </View>
         <View style={commonStyles.row}>
           <View style={styles.aimText1}>
@@ -95,34 +88,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonWrapper: {
-    width: '100%',
-    alignItems: 'center',
-    paddingTop: 250,
-    paddingBottom: 250,
-  },
   title1: {
     fontSize: 65,
   },
   title2: {
     fontSize: 40,
   },
-  button: {
-    width: '70%',
-    height: 50,
-    alignItems: 'center',
-    borderRadius: 15,
-    borderWidth: 2,
-    padding: 10,
-  },
-  buttonText: {
-    fontSize: 25,
-  },
   scrollview: {
     width: '100%',
   },
   aimText1: {
-    paddingLeft:50,
+    paddingLeft: 50,
     paddingRight: 100,
   },
 });
