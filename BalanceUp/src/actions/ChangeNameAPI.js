d8717a77608b53393a3069e7807a70394d5c3cff
hyeone999 <caps026@naver.com>
2022-12-16 09:12:20 +0900

    Test ChangeNameAPI

---
+++ BalanceUp/src/actions/ChangeNameAPI.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const ChangeNameAPI = async userName => {
+  let return_value;
+  await axios
+    .put(
+      'http://ec2-15-165-88-42.ap-northeast-2.compute.amazonaws.com:8080/user/nickname',
+      {
+        nickname: userName,
+      },
+    )
+    .then(response => {
+      console.log(response.data);
+      // 테스트를 위해 임의로 true로 설정해둔 코드
+      return_value = true;
+
+      // 정상적 코드
+      // return_value = response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return_value = true;
+    });
+  return return_value;
+};
+
+// response가 존재하면, return_value = response
+// response가 존재하지 않으면, return value = true
+// (기본값: false)
+
+export default ChangeNameAPI;
