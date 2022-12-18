0e8bdd7f2ab569e3673cb96d12f2e0100c6c68f6
hyeone999 <caps026@naver.com>
2022-12-18 16:35:41 +0900

    ChangeNameAPI Token

--- BalanceUp/src/actions/ChangeNameAPI.js
+++ BalanceUp/src/actions/ChangeNameAPI.js
@@ -7,15 +7,13 @@ const ChangeNameAPI = async userName => {
       'http://ec2-15-165-88-42.ap-northeast-2.compute.amazonaws.com:8080/user/nickname',
       {
         nickname: userName,
+        token: token, // 임시
       },
     )
     .then(response => {
       console.log(response.data);
       // 테스트를 위해 임의로 true로 설정해둔 코드
       return_value = true;
-
-      // 정상적 코드
-      // return_value = response.data;
     })
     .catch(function (error) {
       console.log(error);
@@ -23,9 +21,5 @@ const ChangeNameAPI = async userName => {
     });
   return return_value;
 };
-
-// response가 존재하면, return_value = response
-// response가 존재하지 않으면, return value = true
-// (기본값: false)
 
 export default ChangeNameAPI;
