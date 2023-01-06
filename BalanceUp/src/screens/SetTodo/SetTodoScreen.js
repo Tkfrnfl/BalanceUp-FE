c018578672b7ccd8804416d10220aa7bfef81a16
hyeone999 <caps026@naver.com>
2023-01-06 19:23:59 +0900

    apply design SetTodoScreen

--- BalanceUp/src/screens/SetTodo/SetTodoScreen.js
+++ BalanceUp/src/screens/SetTodo/SetTodoScreen.js
@@ -1,220 +1,121 @@
-import React, {useState, useRef, useEffect} from 'react';
-import {
-  View,
-  Text,
-  Modal,
-  Pressable,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-  Animated,
-  PanResponder,
-  TouchableWithoutFeedback,
-  Image,
-} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
 import {Shadow} from 'react-native-shadow-2';
 import commonStyles from '../../css/commonStyles';
+import FastImage from 'react-native-fast-image';
 
-import modalInnerStyles from '../../css/modalStyles';
-import education from '../../resource/image/education.png';
-import health from '../../resource/image/health.png';
-import life from '../../resource/image/life.png';
-import mental from '../../resource/image/mental.png';
+import education from '../../resource/image/SetTodo/education.png';
+import health from '../../resource/image/SetTodo/health.png';
+import life from '../../resource/image/SetTodo/life.png';
+import mental from '../../resource/image/SetTodo/mental.png';
 
-const SetTodoScreen = ({navigation}) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const [plan, setPlan] = useState({
-    sportPlan: '운동',
-    studyPlan: '학습',
-    dailyPlan: '일상',
-    carePlan: '마음관리',
-  });
-
-  const screenHeight = Dimensions.get('screen').height;
-
-  const panY = useRef(new Animated.Value(screenHeight)).current;
-
-  const translateY = panY.interpolate({
-    inputRange: [-1, 0, 1],
-    outputRange: [-1, 0, 1],
-  });
-
-  const resetBottomSheet = Animated.timing(panY, {
-    toValue: 0,
-    duration: 300,
-    useNativeDriver: true,
-  });
-
-  const closeBottomSheet = Animated.timing(panY, {
-    toValue: screenHeight,
-    duration: 300,
-    useNativeDriver: true,
-  });
-
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: gestureState => panY.setValue(gestureState.dy),
-      onPanResponderRelease: gestureState => {
-        if (gestureState.dy > 0 && gestureState.vy > 1.5) {
-          closeModal();
-        } else {
-          resetBottomSheet.start();
-        }
-      },
-    }),
-  ).current;
-
-  const closeModal = () => {
-    closeBottomSheet.start(() => setIsModalVisible(false));
-  };
-
-  useEffect(() => {
-    if (isModalVisible) {
-      resetBottomSheet.start();
-    }
-  }, [isModalVisible]);
-
-  const goNext = () => {
-    navigation.navigate('Main');
-    setIsModalVisible(false);
-  };
-
-  const goSportPlan = () => {
-    navigation.navigate('Plan', {planText: plan.sportPlan});
-  };
-  const goStudyPlan = () => {
-    navigation.navigate('Plan', {planText: plan.studyPlan});
-  };
-  const goDailyPlan = () => {
-    navigation.navigate('Plan', {planText: plan.dailyPlan});
-  };
-  const goCarePlan = () => {
-    navigation.navigate('Plan', {planText: plan.carePlan});
-  };
-
-  // 하단 탭바 네이게이션
-  const goHome = () => {
-    navigation.navigate('Main');
-  };
-  const goSet = () => {
-    navigation.navigate('Set');
-  };
-  const goLookAll = () => {
-    navigation.navigate('LookAll');
-  };
-  const goMyPage = () => {
-    navigation.navigate('MyPage');
-  };
-
-  const TodoData = [
+const SetTodoScreen = ({navigation: {navigate}}) => {
+  const [disabled, setDisabled] = useState(true);
+  const [selected, setSelected] = useState(new Map());
+  const [todoTitle, setTodoTitle] = useState('');
+  const todoData = [
     {
       id: 1,
       title: '운동',
       subTitle: '체력을 기를래요!',
+      img: health,
     },
     {
       id: 2,
       title: '학습',
       subTitle: '습득력이 높아져요!',
+      img: education,
     },
     {
       id: 3,
       title: '일상',
       subTitle: '지구력이 상승해요!',
+      img: life,
     },
     {
       id: 4,
       title: '마음관리',
       subTitle: '회복력이 좋아져요!',
+      img: mental,
     },
   ];
 
+  // select 되었을 때의 event
+  const onSelect = (id, title) => {
+    if (selected === id) {
+      setSelected(null);
+      setDisabled(true);
+      setTodoTitle('');
+      return;
+    }
+    setSelected(id);
+    setTodoTitle(title);
+    setDisabled(false);
+  };
+
+  console.log(todoTitle);
+
+  const Item = ({id, img, title, subTitle, selected, onSelect}) => {
+    return (
+      <View style={styles.btnSheet}>
+        <Shadow distance={7} startColor="#f3f6f4" offset={[2, 2]}>
+          <TouchableOpacity
+            style={[
+              styles.todoBtn,
+              {borderWidth: selected ? 2 : null},
+              {borderColor: selected ? '#585FFF' : null},
+            ]}
+            activeOpacity={1.0}
+            onPress={() => onSelect(id, title)}>
+            <FastImage source={img} style={styles.btnImg} />
+            <Text style={styles.todoText}>{title}</Text>
+            <Text style={styles.todoSubText}>{subTitle}</Text>
+          </TouchableOpacity>
+        </Shadow>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.topSheet}>
-        <Text style={styles.topTitle}>루틴 설정</Text>
-        <TouchableOpacity onPress={() => setIsModalVisible(!isModalVisible)}>
-          {/* 임시 디자인 버튼 */}
-          <Text style={styles.xBtn}>X</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.subTitle}>
-        <Text style={styles.subText}>
-          내가 키울 루틴의 {'\n'}카테고리가 무엇인가요?
-        </Text>
-      </View>
+      <Text style={styles.title}>
+        내가 키울 루틴의 {'\n'}카테고리가 무엇인가요?
+      </Text>
       {/* 루틴 설정 */}
-      <View style={styles.bntSheet}>
-        <TouchableOpacity style={styles.todoBtn} onPress={goSportPlan}>
-          <Text style={styles.todoText}>{TodoData[0].title}</Text>
-          <Image source={health} style={styles.btnimg1} />
-          <Text style={styles.todoSubText}>{TodoData[0].subTitle}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.todoBtn} onPress={goStudyPlan}>
-          <Text style={styles.todoText}>{TodoData[1].title}</Text>
-          <Image source={education} style={styles.btnimg1} />
-          <Text style={styles.todoSubText}>{TodoData[1].subTitle}</Text>
+      <FlatList
+        data={todoData}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        extraData={selected}
+        renderItem={({item}) => (
+          <Item
+            id={item.id}
+            img={item.img}
+            title={item.title}
+            subTitle={item.subTitle}
+            selected={item.id === selected}
+            onSelect={onSelect}
+          />
+        )}
+      />
+      <View style={{alignItems: 'center', top: 20}}>
+        <TouchableOpacity
+          style={[
+            styles.nextBtn,
+            {backgroundColor: disabled ? '#CED6FF' : '#585FFF'},
+          ]}
+          activeOpacity={1.0}
+          disabled={disabled}
+          onPress={() => navigate('Plan', {planText: todoTitle})}>
+          <Text style={styles.nextBtnText}>다음</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.bntSheet}>
-        <TouchableOpacity style={styles.todoBtn} onPress={goDailyPlan}>
-          <Text style={styles.todoText}>{TodoData[2].title}</Text>
-          <Image source={life} style={styles.btnimg2} />
-          <Text style={styles.todoSubText}>{TodoData[2].subTitle}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.todoBtn} onPress={goCarePlan}>
-          <Text style={styles.todoText}>{TodoData[3].title}</Text>
-          <Image source={mental} style={styles.btnimg1} />
-          <Text style={styles.todoSubText}>{TodoData[3].subTitle}</Text>
-        </TouchableOpacity>
-      </View>
-      {/* 모달 구현 코드 */}
-      <Modal
-        visible={isModalVisible}
-        animationType={'fade'}
-        transparent={true}
-        statusBarTranslucent={true}>
-        <Pressable
-          style={modalInnerStyles.modalOverlay}
-          onPress={() => setIsModalVisible(!isModalVisible)}>
-          <TouchableWithoutFeedback>
-            <Animated.View
-              style={{
-                ...modalInnerStyles.bottomSheetContainer,
-                transform: [{translateY: translateY}],
-              }}
-              {...panResponder.panHandlers}>
-              {/* 모달에 들어갈 내용을 아래에 작성 */}
-              <Text style={modalInnerStyles.modalTitle}>
-                루틴 설정을 종료하시겠어요?
-              </Text>
-              <View style={modalInnerStyles.modalFlex}>
-                <TouchableOpacity
-                  style={modalInnerStyles.noBtn}
-                  onPress={() => setIsModalVisible(false)}>
-                  <Text style={modalInnerStyles.noText}>아니요</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={modalInnerStyles.nextBtn}
-                  onPress={goNext}>
-                  <Text style={modalInnerStyles.nextText}>
-                    다음에 정할래요!
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </Animated.View>
-          </TouchableWithoutFeedback>
-        </Pressable>
-      </Modal>
 
       {/* 하단 탭바 */}
-      <View style={{flex: 1, marginTop: 98}}>
+      <View style={{flex: 1, marginTop: 58}}>
         <Shadow distance={3}>
           <View style={commonStyles.bottomTabSheet}>
-            <TouchableOpacity onPress={goHome}>
+            <TouchableOpacity onPress={() => navigate('Main')}>
               <Text
                 style={[
                   commonStyles.commonText,
@@ -226,13 +127,13 @@ const SetTodoScreen = ({navigation}) => {
                 홈
               </Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={goSet}>
+            <TouchableOpacity onPress={() => navigate('Set')}>
               <Text style={commonStyles.selectText}>작성</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={goLookAll}>
+            <TouchableOpacity onPress={() => navigate('LookAll')}>
               <Text style={commonStyles.commonText}>루틴</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={goMyPage}>
+            <TouchableOpacity onPress={() => navigate('MyPage')}>
               <Text style={commonStyles.commonText}>마이페이지</Text>
             </TouchableOpacity>
           </View>
@@ -247,67 +148,55 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1,
   },
-  topSheet: {
-    alignItems: 'center',
-    marginTop: 40,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
-  topTitle: {
-    fontSize: 15,
-    fontWeight: '600',
-    color: '#000',
-  },
-  xBtn: {
-    marginLeft: 120,
-    marginRight: 20,
-    fontSize: 30,
-    fontWeight: '400',
-    color: '#000',
-  },
-  subTitle: {
-    marginTop: 30,
-    marginLeft: 30,
-    marginBottom: 20,
-  },
-  subText: {
-    color: '#000',
+  title: {
     fontSize: 22,
-    fontWeight: 'bold',
+    fontWeight: '600',
+    color: '#232323',
+    textAlign: 'center',
+    marginTop: 70,
+    marginBottom: 50,
+  },
+  btnSheet: {
+    flexDirection: 'column',
+    marginLeft: 25,
+    margin: 10,
   },
   todoBtn: {
-    backgroundColor: '#E8EBFD',
-    width: '40%',
-    marginRight: 8,
-    marginTop: 10,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    width: 160,
+    height: 140,
+    marginRight: -10,
+    borderRadius: 5,
   },
   todoText: {
-    textAlign: 'center',
-    marginTop: 20,
-    fontSize: 25,
     fontWeight: 'bold',
+    fontSize: 20,
     color: '#000',
+    marginTop: 5,
   },
   todoSubText: {
-    textAlign: 'center',
-    fontSize: 15,
-    marginTop: 80,
-    marginBottom: 20,
-    fontWeight: 'bold',
-    color: '#000',
+    fontWeight: '500',
+    color: '#888888',
+    fontSize: 12,
+    marginTop: 3,
   },
-  bntSheet: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+  btnImg: {
+    width: 80,
+    height: 60,
+    marginTop: 10,
+    marginBottom: 5,
   },
-  btnimg1: {
-    marginBottom: -100,
-    marginTop: -20,
+  nextBtn: {
+    width: 336,
+    height: 48,
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 5,
   },
-  btnimg2: {
-    marginBottom: -80,
-    marginTop: -10,
-    marginLeft: 20,
+  nextBtnText: {
+    fontSize: 16,
+    color: '#fff',
   },
 });
 
