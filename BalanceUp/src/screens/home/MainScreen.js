eef307cfdf7f06bb6b2c2958ebf0080b9f51704a
hyeone999 <caps026@naver.com>
2023-03-03 11:37:12 +0900

    Modifying Text

--- BalanceUp/src/screens/home/MainScreen.js
+++ BalanceUp/src/screens/home/MainScreen.js
@@ -408,7 +408,7 @@ const MainScreen = ({navigation: {navigate}}) => {
             second: 0,
             millisecond: 0,
           });
-          var correctHours = moment.duration('09:00:00');  //시간보정
+          var correctHours = moment.duration('09:00:00'); //시간보정
           m.subtract(correctHours);
           m.toDate();
           var tmpM = new Date(m);
@@ -447,7 +447,7 @@ const MainScreen = ({navigation: {navigate}}) => {
               // repeatType: 'week',
               //date: new Date(Date.now() + 30 * 1000), // 시간대 에러날시 서버시간 체크후 보정
             });
-             PushNotification.getScheduledLocalNotifications(callback => {
+            PushNotification.getScheduledLocalNotifications(callback => {
               console.log(callback);
             });
           } else {
@@ -490,7 +490,7 @@ const MainScreen = ({navigation: {navigate}}) => {
           <TouchableOpacity
             onPress={() => navigate('Guide')}
             activeOpacity={1.0}>
-            <Text style={styles.guideText}>키움 성장 가이드</Text>
+            <Text style={styles.guideText}>KEYUM 성장 가이드</Text>
           </TouchableOpacity>
 
           {/* 레벨 progressBar */}
